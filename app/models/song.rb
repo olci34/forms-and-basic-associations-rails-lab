743d0df5145a84878ee5a98c12c21335dd829d8a
github username <github email address>
2021-03-17 15:55:39 -0400

    commit tree 773d5655394ea0107d20f0cab434e0eb025bb2f2

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,30 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def artist_name=(name)
+    artist = Artist.find_or_create_by(name: name)
+    self.artist_id = artist.id
+  end
+
+  def artist_name
+    self.artist ? self.artist.name : nil
+  end
+
+  def note_contents=(contents)
+    contents.each do |content|
+      if !content.strip.empty?
+        note = Note.create(content: content)
+        self.notes << note
+        # note.song_id = self.id
+        # self.notes.build(content:content)
+      end
+    end
+  end
+
+  def note_contents
+    self.notes.map { |note| note.content }
+  end
 end
